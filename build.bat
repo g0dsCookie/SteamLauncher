61275fd2fc14e45c4d75f356a492cbd50d254806
g0dsCookie <g0dscookie@cookieprojects.de>
2017-07-23 16:03:23 +0200

    Restructured project

---
+++ build.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo "This batch should be run with the Developer Command Line from Visual Studio."
+echo "Also this batch needs 7z to be in your path."
+pause
+
+echo "Building SteamLauncher..."
+msbuild Build.proj
+
+echo "Zipping Folder..."
+cd bin
+7z a -tzip -mx9 -y SteamLauncher.zip *
+cd ..
+
+echo "Done!"
+pause
